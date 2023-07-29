ec944ac2dc4bc8c8ca5f2c45c9f620ee5324b07d
Shubham Ghare <shubham.ghare@myashida.com>
2023-07-29 12:06:34 +0530

    deleted and add iec standard

--- pecas/lab_pecas/doctype/iecstandard/iecstandard.js
+++ pecas/lab_pecas/doctype/iecstandard/iecstandard.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, shubham and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('ICEStandard', {
+frappe.ui.form.on('IECStandard', {
 	// refresh: function(frm) {
 
 	// }
--- pecas/lab_pecas/doctype/iecstandard/test_iecstandard.py
+++ pecas/lab_pecas/doctype/iecstandard/test_iecstandard.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestICEStandard(FrappeTestCase):
+class TestIECStandard(FrappeTestCase):
 	pass
